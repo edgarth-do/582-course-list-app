bc8eb79cc1add0d331b0dcf122948363b2d09438
edgardo <2295444@edu.vaniercollege.qc.ca>
2023-08-03 09:49:18 -0400

    Initiate commiting

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  <h1>Course List App</h1>
+  <p>{{ count }}</p>
+  <CourseItem @count-to-parent="addOne" />
+  <courses
+    :id="profile"
+    :course="course"
+    :credits="credits"
+    :location="location"
+    :availability="availability"
+  ></courses>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import CourseItem from "./components/CourseItem.vue";
 
 export default {
   name: "App",
+  data() {
+    return {
+      count: 0,
+      availability: true,
+      description: "av-label",
+      nav: "av-label disabled",
+      age: 25,
+      courses: { id: 1, course: "VUE", credits: 20, location: "south" },
+      // courses: [
+      //   { id: 1, course: "VUE", credits: 20, location: "south" },
+      //   { id: 2, course: "Powershell", credits: 35, location: "west" },
+      //   { id: 3, course: "Sourcetree", credits: 18, location: "west" },
+      // ],
+    };
+  },
   components: {
-    HelloWorld,
+    CourseItem,
+  },
+  methods: {
+    addOne() {
+      this.count++;
+    },
   },
 };
 </script>
@@ -22,5 +52,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.card {
+  display: inline-block;
+  width: 200px;
+  border: 1px solid #2c3e50;
+  border-radius: 9px;
 }
 </style>
---
+++ src/components/CourseItem.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="card">
+    <h2>Course name:</h2>
+    <div>Credits:</div>
+    <div>Id:</div>
+    <div>Teacher:</div>
+    <div>Location:</div>
+    <div>Description:</div>
+
+    <!-- <p v-if="(count = 0)">summit</p>
+      <p v-else-if="count > 1 && count < 20">Allowed</p>
+      <p v-else>Full</p> -->
+
+    <button @click="innerAdd">Add</button>
+    <button @click="outerAdd">Add to parent</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CourseItem",
+  props: {
+    id: {
+      type: Number,
+      required: true,
+    },
+    course: {
+      type: String,
+      required: true,
+    },
+    credits: {
+      type: Number,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    innerAdd() {
+      this.count++;
+    },
+    outerAdd() {
+      this.$emit("count-to-parent");
+    },
+  },
+};
+</script>
